e1efa77fe44fcfef70b1f95fa59da4d3d8ffba3e
Viacheslav Rostovtsev <virost@google.com>
2026-03-02 13:47:52 -0800

    chore: tests for retry jitter

--- gapic-common/test/gapic/common/retry_policy_test.rb
+++ gapic-common/test/gapic/common/retry_policy_test.rb
@@ -21,17 +21,18 @@ class RetryPolicyTest < Minitest::Test
   def test_init_with_values
     retry_policy = Gapic::Common::RetryPolicy.new(
       initial_delay: 2, max_delay: 20, multiplier: 1.7,
-      retry_codes: [GRPC::Core::StatusCodes::UNAVAILABLE], timeout: 600
+      retry_codes: [GRPC::Core::StatusCodes::UNAVAILABLE], timeout: 600, jitter: 0
     )
     assert_equal 2, retry_policy.initial_delay
     assert_equal 20, retry_policy.max_delay
     assert_equal 1.7, retry_policy.multiplier
     assert_equal [GRPC::Core::StatusCodes::UNAVAILABLE], retry_policy.retry_codes
     assert_equal 600, retry_policy.timeout
+    assert_equal 0, retry_policy.jitter
   end
 
   def test_perform_delay_increment_delay
-    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 1, max_delay: 5, multiplier: 1.3
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 1, max_delay: 5, multiplier: 1.3, jitter: 0
     retry_policy.start! mock_delay: true
     retry_policy.perform_delay!
     refute_equal retry_policy.initial_delay, retry_policy.delay
@@ -39,7 +40,7 @@ class RetryPolicyTest < Minitest::Test
   end
 
   def test_perform_delay_retry_common_logic
-    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 3, max_delay: 10, multiplier: 2
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 3, max_delay: 10, multiplier: 2, jitter: 0
     retry_policy.define_singleton_method :retry? do
       true
     end
@@ -49,7 +50,7 @@ class RetryPolicyTest < Minitest::Test
   end
 
   def test_perform_delay_retry_error_logic
-    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 5, max_delay: 30, multiplier: 3
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 5, max_delay: 30, multiplier: 3, jitter: 0
     retry_policy.define_singleton_method :retry_error? do |_error|
       true
     end
@@ -59,16 +60,59 @@ class RetryPolicyTest < Minitest::Test
   end
 
   def test_max_delay_limit
-    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 10, max_delay: 12, multiplier: 1.5
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 10, max_delay: 12, multiplier: 1.5, jitter: 0
     retry_policy.start! mock_delay: true
     retry_policy.perform_delay!
     assert_equal 12, retry_policy.delay
   end
 
   def test_retry_policy_deadline_init
-    Process.stub :clock_gettime, 123456789.0 do
-      retry_policy = Gapic::Common::RetryPolicy.new timeout: 10
-      assert_equal 123456799.0, retry_policy.send(:deadline)
+    Process.stub :clock_gettime, 123_456_789.0 do
+      retry_policy = Gapic::Common::RetryPolicy.new timeout: 10, jitter: 0
+      assert_equal 123_456_799.0, retry_policy.send(:deadline)
     end
+  end
+
+  def test_negative_jitter_raises_error
+    assert_raises ArgumentError do
+      Gapic::Common::RetryPolicy.new jitter: -1.0
+    end
+  end
+
+  def test_jitter_is_added
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 5, max_delay: 10, multiplier: 2, jitter: 2.0
+
+    delays_performed = []
+    retry_policy.start! mock_delay: ->(d) { delays_performed << d }
+
+    Kernel.stub :rand, 1.5 do
+      retry_policy.perform_delay!
+    end
+
+    # base delay = 5, rand = 1.5, actual = 6.5
+    assert_equal 6.5, delays_performed.first
+    # internal state should remain deterministic
+    assert_equal 10, retry_policy.delay
+  end
+
+  def test_jitter_bounds
+    retry_policy = Gapic::Common::RetryPolicy.new initial_delay: 5, max_delay: 6, multiplier: 2, jitter: 2.0
+
+    delays_performed = []
+    retry_policy.start! mock_delay: ->(d) { delays_performed << d }
+
+    Kernel.stub :rand, 2.0 do
+      retry_policy.perform_delay!
+    end
+
+    # base delay is 5, rand is 2.0 = 7.0, capped at max_delay of 6
+    assert_equal 6, delays_performed.first
+
+    Kernel.stub :rand, 2.0 do
+      retry_policy.perform_delay!
+    end
+
+    # internal state is now 10, rand is 2.0 = 12.0, capped at max_delay of 6
+    assert_equal 6, delays_performed.last
   end
 end
